9fb4a13b0e15d34845443ccfc132072db09f9e3c
vagrant <vagrant@localhost.localdomain>
2018-09-20 00:55:48 +0000

    build rate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911030128) do
+ActiveRecord::Schema.define(version: 20180920004933) do
+
+  create_table "rates", force: :cascade do |t|
+    t.string "rate_type"
+    t.integer "amount"
+    t.integer "rank"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "weekday"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.string "reserve_no"
