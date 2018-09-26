630de3f608d163ca67582cf127bc1fd8d901412b
vagrant <vagrant@localhost.localdomain>
2018-09-26 11:40:32 +0000

    Room_Rate Display

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,3 +1,7 @@
 class Rate < ApplicationRecord
   belongs_to :room_type
+  validates :rate_type, presence: true
+  validates :amount, presence: true
+  validates :rank, presence: true
+  validates :room_type_id, presence: true
 end
