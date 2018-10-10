4423a7f97d7baf6b718c99872396ac00c135eff9
vagrant <vagrant@localhost.localdomain>
2018-10-10 06:04:53 +0000

    Add Reserve Form (rough)

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,21 +3,7 @@ class Reservation < ApplicationRecord
   validates :guest_no, presence: true
   validates :guest_phone, presence: true
   validates :guest_mail, presence: true
-  validates :check_in_date, presence: true, :in_date_check
-  validates :check_out_date, presence: true, :out_date_check
-  validates :room_id, presence: true, uniqueness: { scope: [:duration_of_stay]}
   validates :guest_count, presence: true
 
-  def in_date_check
-    check_in_date => Date.today
-  end
-
-  def out_date_check
-    check_out_date => check_in_date
-  end
-
-  def duration_of_stay
-    check_in_date..check_out_date
-  end
 
 end
