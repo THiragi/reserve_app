2e3ab36a02b0d56e2dd7e1240d2748988adb46fd
vagrant <vagrant@localhost.localdomain>
2018-11-18 23:17:35 +0000

    Corrected Admin Path

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,12 +2,4 @@ class Admin::BaseController < ActionController::Base
   layout 'base'
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-    admin_reservations_path
-  end
-
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
-  end
-
 end
