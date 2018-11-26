f912b14ab3010aba8f29d6574b82d8c1c87ea792
vagrant <vagrant@localhost.localdomain>
2018-11-26 07:43:07 +0000

    Revise Date Check Validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -17,7 +17,7 @@ class Reservation < ApplicationRecord
   validate :reserves_overlap
 
   def reserves_overlap
-     reservations = Reservation.where(room_id: self.id).where('check_in_date <= ? AND ? <= check_out_date', self.check_out_date, self.check_in_date)
+     reservations = Reservation.where(room_id: self.id).where('check_in_date < ? AND ? < check_out_date', self.check_out_date, self.check_in_date)
      reservations = reservations.where.not(id: self.id) if !self.new_record?
 
      errors.add(:check_in_date, "その期間はすでに予約が入っています") if reservations.any?
@@ -45,7 +45,7 @@ class Reservation < ApplicationRecord
   end
 
   def self.date_check(room_id, in_date, out_date)
-    Reservation.where(room_id: room_id).where('check_in_date <= ? AND ? <= check_out_date', out_date, in_date)
+    Reservation.where(room_id: room_id).where('check_in_date < ? AND ? < check_out_date', out_date, in_date)
   end
 
 end
