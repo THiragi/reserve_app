4e519491357ee5af5b364f5ea4e289782f52ef6d
vagrant <vagrant@localhost.localdomain>
2019-01-18 15:35:42 +0000

    Added Prev & Next Month on AGG

--- app/controllers/admin/aggregations_controller.rb
+++ app/controllers/admin/aggregations_controller.rb
@@ -1,12 +1,22 @@
 class Admin::AggregationsController < Admin::BaseController
   def index
+    @reservations = Reservation.all
+    @today = Date.today
   end
+
+
 
   def month
   end
 
   def day
     @reservations = Reservation.all
+
+    if params[:today].blank?
+      @today = Date.today
+    else
+      @today = Date.parse(params[:today])
+    end
   end
 
 end
